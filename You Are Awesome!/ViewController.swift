d635506198374b8544fe6aa0d9ac1d8eadc5a086
Mau Garcia <mauricio.rodrigo.gg@gmail.com>
2024-03-12 22:30:16 -0700

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Mauricio R Garcia on 3/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
